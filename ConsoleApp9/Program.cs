a56384916a580f3bcd3449bbf33458a091bacd5b
pc <pc@DESKTOP-P40ILKJ>
2022-09-04 10:45:50 +0100

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+        }
+
+        static void CompoundInterest()
+        {
+            double[] babaAlajo = { 1000, 2000, 500, 1000, 3000, 700, 900 };
+
+            //find the total
+            double total = 0;
+            for (int i = 0; i < babaAlajo.Length; i++)
+            {
+                total += babaAlajo[i];
+                Console.WriteLine($"the principal balance of babaalajo's savings is {total}");
+                Console.ReadLine();
+                
+
+            }
+        }
+    }
+}
